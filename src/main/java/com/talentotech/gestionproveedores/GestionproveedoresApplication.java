4bcb234850bb4e07ec4964897d9120ffc6e7270a
Ivosaja <ivoliobel@gmail.com>
2025-11-27 17:56:50 -0300

    Creacion de proyecto base

---
+++ src/main/java/com/talentotech/gestionproveedores/GestionproveedoresApplication.java
@@ -0,0 +1,13 @@
+package com.talentotech.gestionproveedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionproveedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionproveedoresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentotech/gestionproveedores/GestionproveedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentotech.gestionproveedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionproveedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
