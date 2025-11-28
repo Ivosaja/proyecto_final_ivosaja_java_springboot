3fa1fde3bd1c3854baec5cb427e60695f3b0b5dd
Ivosaja <ivoliobel@gmail.com>
2025-11-28 16:14:28 -0300

    ProveedoresController: Creacion de controller base

---
+++ src/main/java/com/talentotech/gestionproveedores/controller/ProveedoresController.java
@@ -0,0 +1,10 @@
+package com.talentotech.gestionproveedores.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/proveedores")
+public class ProveedoresController {
+
+}
