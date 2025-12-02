2e84e03fa39602780fc5443a5320e91fbd036e1f
Ivosaja <ivoliobel@gmail.com>
2025-12-01 22:26:37 -0300

    ProveedorService: Creacion de interfaz que brinda metodos para que la clase ProveedorServiceImpl los implemente

--- src/main/java/com/talentotech/gestionproveedores/service/ProveedorService.java
+++ src/main/java/com/talentotech/gestionproveedores/service/ProveedorService.java
@@ -1,57 +1,24 @@
 package com.talentotech.gestionproveedores.service;
 
 import java.util.List;
-
-import org.springframework.stereotype.Service;
-
 import com.talentotech.gestionproveedores.model.Proveedor;
-import com.talentotech.gestionproveedores.repository.ProveedorRepository;
 
-// Marco la clase como servicio de Spring
-@Service
-public class ProveedorService {
+// Interfaz que brinda metodos a ser implementados
+public interface ProveedorService {
 
-    private final ProveedorRepository proveedorRepository;
+    // Obtener todos los proveedores
+    List<Proveedor> obtenerProveedores();
 
-    public ProveedorService(ProveedorRepository proveedorRepository){
-        this.proveedorRepository = proveedorRepository;
-    }
+    // Obtener un proveedor por ID
+    Proveedor obtenerProveedorPorId(Long id);
 
-    // ----------------------- OBTENER PROVEEDORES ----------------------- //
-    // Metodo encargado de devolver todos los proveedores de la BD
-    public List<Proveedor> obtenerProveedores(){
-        return this.proveedorRepository.findAll();
-    }
-
-    // ----------------- OBTENER PROVEEDOR POR ID ----------------- //
-    // Metodo encargado de devolver un proveedor por ID de la BD
-    public Proveedor obtenerProveedorPorId(Long id){
-        return this.proveedorRepository.findById(id).orElseThrow(() -> new IllegalStateException("Proveedor con ID: " + id + " no encontrado"));
-    }
-
-    // ---------------------- CREAR PROVEEDOR ---------------------- //
-    // Metodo encargado de crear un proveedor en la BD
-    public Proveedor creaProveedor(Proveedor p){
-        return this.proveedorRepository.save(p);
-    }
-
-    // ------------- ACTUALIZAR PROVEEDOR ------------- //
-    // Metodo encargado de actualizar un proveedor de la BD
-    public void actualizarProveedor(Proveedor nuevoProveedor, Long id){
-        Proveedor proveedorParaActualizar = this.proveedorRepository.findById(id)
-            .orElseThrow(() -> new IllegalStateException("Proveedor con ID: " + id + " no encontrado"));
-        
-        proveedorParaActualizar.setNombre(nuevoProveedor.getNombre());
-        proveedorParaActualizar.setApellido(nuevoProveedor.getApellido());
-        this.proveedorRepository.save(proveedorParaActualizar);
-        
-    }
-
-    // ------------------- ELIMINAR PROVEEDOR ------------------- //
-    // Metodo encargado de eliminar un proveedor de la BD
-    public void eliminarProveedor(Long id){
-        this.proveedorRepository.deleteById(id);
-    }
-
+    // Crear un nuevo proveedor
+    Proveedor crearProveedor(Proveedor p);
+    
+    // Actualizar un proveedor por ID
+    Proveedor actualizarProveedor(Proveedor nuevoProveedor, Long id);
+    
+    // Eliminar un proveedor por ID
+    void eliminarProveedor(Long id);
 
 }
