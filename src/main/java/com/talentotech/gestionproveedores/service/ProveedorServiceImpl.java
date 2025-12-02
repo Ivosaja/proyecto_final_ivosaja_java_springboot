1226b89eeea5255ee31aaddfe4ed9ed172b48229
Ivosaja <ivoliobel@gmail.com>
2025-12-02 18:55:35 -0300

    Service: Desarrollo de metodos de ProveedorServiceImpl

---
+++ src/main/java/com/talentotech/gestionproveedores/service/ProveedorServiceImpl.java
@@ -0,0 +1,78 @@
+package com.talentotech.gestionproveedores.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.talentotech.gestionproveedores.exception.ProveedorInvalidoException;
+import com.talentotech.gestionproveedores.exception.ProveedorNoEncontradoException;
+import com.talentotech.gestionproveedores.model.Proveedor;
+import com.talentotech.gestionproveedores.repository.ProveedorRepository;
+
+// Marco la clase como servicio de Spring
+@Service
+public class ProveedorServiceImpl implements ProveedorService{
+
+    private final ProveedorRepository proveedorRepository;
+
+    public ProveedorServiceImpl(ProveedorRepository proveedorRepository){
+        this.proveedorRepository = proveedorRepository;
+    }
+
+    // ----------------------- OBTENER PROVEEDORES ----------------------- //
+    // Metodo encargado de devolver todos los proveedores de la BD
+    @Override
+    public List<Proveedor> obtenerProveedores(){
+        return this.proveedorRepository.findAll();
+    }
+
+    // ----------------- OBTENER PROVEEDOR POR ID ----------------- //
+    // Metodo encargado de devolver un proveedor por ID de la BD
+    @Override
+    public Proveedor obtenerProveedorPorId(Long id){
+        return this.proveedorRepository.findById(id)
+            .orElseThrow(() -> new ProveedorNoEncontradoException("Proveedor con ID: " + id + " no encontrado"));
+    }
+
+    // ---------------------- CREAR PROVEEDOR ---------------------- //
+    // Metodo encargado de crear un proveedor en la BD
+    @Override
+    public Proveedor crearProveedor(Proveedor p){
+        if(p.getNombre() == null || p.getNombre().isBlank()){
+            throw new ProveedorInvalidoException("Campo invalido. El nombre del proveedor no puede ser vacio");
+        }
+        if(p.getApellido() == null || p.getApellido().isBlank()){
+            throw new ProveedorInvalidoException("Campo invalido. El apellido del proveedor no puede ser vacio");
+        }
+        return this.proveedorRepository.save(p);
+    }
+
+    // ------------- ACTUALIZAR PROVEEDOR ------------- //
+    // Metodo encargado de actualizar un proveedor de la BD
+    @Override
+    public Proveedor actualizarProveedor(Proveedor nuevoProveedor, Long id){
+        Proveedor p = this.proveedorRepository.findById(id)
+            .orElseThrow(() -> new ProveedorNoEncontradoException("Proveedor con ID: " + id + " no encontrado"));
+        
+        if(nuevoProveedor.getNombre() == null || nuevoProveedor.getNombre().isBlank()){
+            throw new ProveedorInvalidoException("Campo invalido. El nombre del proveedor no puede ser vacio");
+        }
+        if(nuevoProveedor.getApellido() == null || nuevoProveedor.getApellido().isBlank()){
+            throw new ProveedorInvalidoException("Campo invalido. El apellido del proveedor no puede ser vacio");
+        }
+
+        p.setNombre(nuevoProveedor.getNombre());
+        p.setApellido(nuevoProveedor.getApellido());
+        return this.proveedorRepository.save(p);
+    }
+
+    // ------------------- ELIMINAR PROVEEDOR ------------------- //
+    // Metodo encargado de eliminar un proveedor de la BD
+    @Override
+    public void eliminarProveedor(Long id){
+        Proveedor p = this.proveedorRepository.findById(id)
+            .orElseThrow(() -> new ProveedorNoEncontradoException("Proveedor con ID: " + id + " no encontrado"));
+        this.proveedorRepository.deleteById(p.getId());
+    }
+
+}
