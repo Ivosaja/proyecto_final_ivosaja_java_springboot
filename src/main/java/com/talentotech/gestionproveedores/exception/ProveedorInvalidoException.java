b744859b1a11f0aef4a347d405023b3ae0a13ec3
Ivosaja <ivoliobel@gmail.com>
2025-12-03 14:51:43 -0300

    Eliminacion de Excepcion Custom (reemplazada por validaciones de Jakarta)

--- src/main/java/com/talentotech/gestionproveedores/exception/ProveedorInvalidoException.java
+++
@@ -1,8 +0,0 @@
-package com.talentotech.gestionproveedores.exception;
-
-// Excepcion personalizada en caso de que mande un Proveedor invalido por Body
-public class ProveedorInvalidoException extends RuntimeException {
-    public ProveedorInvalidoException(String mensaje){
-        super(mensaje);
-    }
-}
